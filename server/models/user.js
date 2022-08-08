c0a5ebfa27c464e2cfe5adb8e2bad648c1601226
Paulo Faria <santos.paulo.rf@gmail.com>
2022-08-08 06:04:14 -0300

    creating schemas and models

---
+++ server/models/user.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+const validator = require("validator")
+require ("dotenv").config();
+
+const userSchema = mongoose.Schema ({
+
+    email:{
+        type: String,
+        required: true, 
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validator(value){
+            if(!validator.isEmail(value)){
+                throw new Error ("Invalid Email")
+            }
+        }
+    },
+    password:{
+        type: String,
+        required: true, 
+        trim: true
+    },
+    role:{
+        type: String,
+        enum: ["user", "admin"],
+        default: "user"
+    },
+    firstname:{
+        type: String,
+        maxLength: 100,
+        trim: true
+    },
+    lastname:{
+        type: String,
+        maxLength: 100,
+        trim: true
+    },
+    age:{
+        type: Number
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    },
+    verified:{
+        type: Boolean,
+        default: false
+    }
+
+})
+
+{/* METHODS */}
+
+
+{/* MIDDLEWARES */}
+
+
+{/* MODELS */}
+const User = mongoose.model("User", userSchema );
+
+{/* EXPORT */}
+module.exports = { User }
