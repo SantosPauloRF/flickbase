afac158354e863b6412637225861aa8d2d54b5cf
Paulo Faria <santos.paulo.rf@gmail.com>
2022-08-21 13:51:07 -0300

    Loging form created.

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./components/home";
 import Header from "./components/navigation/header";
 import MainLayout from "./hoc/mainLayout";
+import Auth from "./components/auth";
 
 function Router() {
   return (
@@ -9,6 +10,7 @@ function Router() {
       <Header />
       <MainLayout>
         <Routes>
+          <Route path="/auth" element={<Auth />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </MainLayout>
